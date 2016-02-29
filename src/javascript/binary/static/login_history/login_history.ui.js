a3e39d8abf636404233198bd286bd33833167aef
root <apoorv@binary.com>
2016-02-29 05:53:17 +0000

    Added Browser Names

--- src/javascript/binary/static/login_history/login_history.ui.js
+++ src/javascript/binary/static/login_history/login_history.ui.js
@@ -25,17 +25,32 @@ var LoginHistoryUI = (function(){
     }
     
     function createRow(data){
-        var history = data['environment'].split(' ');
+        var userAgent = data['environment'];
+        var history = userAgent.split(' ');
         var timestamp = history[0];
         var ip = history[2].split('=')[1];
-        var browser = history[4].split('=')[1] + " " + 
-                        history[5] + " " + history[6] + " " + history[7] + " " + 
-                        history[8] + " " + history[9] + " " + history[10] + " " + 
-                        history[11] + " " + history[12] + " " + history[13] +
-                        " " + history[14];
+        var browser = "Unknown";
+        var verOffset, ver;
+        if ((verOffset = userAgent.indexOf("OPR")) != -1){
+            browser = "Opera";
+            ver = userAgent.substring(verOffset+4);
+        } else if ((verOffset = userAgent.indexOf("Chrome")) != -1){
+            browser = "Chrome";
+            ver = userAgent.substring(verOffset+7);
+        } else if ((verOffset = userAgent.indexOf("Safari")) != -1){
+            browser = "Safari";
+            ver = userAgent.substring(verOffset+7);
+        } else if ((verOffset = userAgent.indexOf("Firefox")) != -1){
+            browser = "Firefox";
+            ver = userAgent.substring(verOffset+8);
+        } else if ((verOffset = userAgent.indexOf("MSIE")) != -1){
+            browser = "Internet Explorer";
+            ver = userAgent.substring(verOffset+5);
+        }
         var status = data['status'] === 1 ? text.localize('Successful') : text.localize('Failed');
         var $row = Table.createFlexTableRow([browser, timestamp, ip, status], columns, "data");
         $row.children(".timestamp").first().append('<br>' + history[1]);
+        $row.children(".browser").first().append('<br>' + ver);
         return $row[0];
     }
     
