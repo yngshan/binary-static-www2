85b1c5e32f71588a30ae10c578f0647b23239b22
Jun Bon Kok <junbon@binary.com>
2016-03-08 17:41:03 +0800

    allowed symbols for japan

--- src/javascript/binary_japan/symbols.js
+++ src/javascript/binary_japan/symbols.js
@@ -5,9 +5,20 @@ if(typeof is_japan === 'function'){
 		details:{
 			value:function(data){
 				var active_symbols = [];
+                                var allowed_symbols = {
+                                    frxAUDJPY: 1,
+                                    frxAUDUSD: 1,
+                                    frxEURGBP: 1,
+                                    frxEURJPY: 1,
+                                    frxEURUSD: 1,
+                                    frxGBPJPY: 1,
+                                    frxGBPUSD: 1,
+                                    frxUSDCAD: 1,
+                                    frxUSDJPY: 1
+                                };
 
 				data.active_symbols.forEach(function(symbol){
-					if(symbol.market==='forex' && symbol.submarket==='major_pairs'){
+					if (typeof allowed_symbols[symbol.symbol] !== 'undefined'){
 						active_symbols.push(symbol);
 					}
 				});
