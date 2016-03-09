f97ead9ed1916f85cdf62ceb17407a7c7e0a9680
negar <negar@binary.com>
2016-03-09 15:05:54 +0800

    update tel field for japan

--- src/javascript/binary/static/japan_acc_opening/japan_acc_opening.ui.js
+++ src/javascript/binary/static/japan_acc_opening/japan_acc_opening.ui.js
@@ -120,7 +120,16 @@ var JapanAccOpeningUI = function () {
       window.accountErrorCounter++;
     }
 
-    ValidAccountOpening.checkTel(elementObj['tel'], errorObj['tel']);
+    if (tel.value.replace(/\+| /g,'').length < 6) {
+      errorTel.innerHTML = Content.errorMessage('min', 6);
+      Validate.displayErrorMessage(errorTel);
+      window.accountErrorCounter++;
+    } else if (!/^\+?[0-9\s-]{6,35}$/.test(tel.value)){
+      initializeValues();
+      errorTel.innerHTML = Content.errorMessage('reg', [numbers, space, hyphen]);
+      Validate.displayErrorMessage(errorTel);
+      window.accountErrorCounter++;
+    }
     ValidAccountOpening.checkAnswer(elementObj['answer'], errorObj['answer']);
 
     if (!/^\d+$/.test(elementObj['limit'].value)) {
