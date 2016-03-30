49d57673d69b709a7868afd0b26a95345210b8dd
qingwei <qingwei@binary.com>
2016-03-30 12:51:28 +0800

    Fix bugs

--- src/javascript/binary/static/lost_password/lost_password.init.js
+++ src/javascript/binary/static/lost_password/lost_password.init.js
@@ -4,9 +4,10 @@ var LostPassword = (function() {
     var hiddenClass = "invisible";
 
     function submitEmail() {
-        var emailInput = $('#lp_email').text();
+        var emailInput = $('#lp_email').val();
+
         if (emailInput === '') {
-            $(".email_error").removeClass(hiddenClass);
+            $("#email_error").removeClass(hiddenClass);
         } else {
             BinarySocket.send({verify_email: emailInput, type: 'reset_password'});
         }
@@ -18,11 +19,14 @@ var LostPassword = (function() {
         }
     }
     
-    function lostPasswordWSHandler(response) {
+    function lostPasswordWSHandler(msg) {
+        var response = JSON.parse(msg.data);
         var type = response.msg_type;
         switch (type) {
             case 'verify_email': if (response.verify_email === 1) {
-                load_with_pjax('user/lost_passwordws_email_sent');
+                load_with_pjax('lost_passwordws_email_sent');
+            } else if (response.error) {
+                $("#email_error").removeClass(hiddenClass).text(text.localize('Invalid email format'));
             }
                 break;
             default: return;
@@ -30,6 +34,17 @@ var LostPassword = (function() {
     }
 
     function init() {
-        
+        $('#submit').click(function() {
+            submitEmail();
+        });
+
+        $('#lp_email').change(function(ev) {
+            onEmailInput(ev.target.value);
+        });
     }
+
+    return {
+        lostPasswordWSHandler: lostPasswordWSHandler,
+        init: init
+    };
 }());
