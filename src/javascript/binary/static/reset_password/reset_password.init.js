e069000471a9ba82799d4be3da71ea49660b0f13
binary-hamedanchi <hamedanchi@gmail.com>
2016-04-11 12:17:30 +0800

    reset password

--- src/javascript/binary/static/reset_password/reset_password.init.js
+++ src/javascript/binary/static/reset_password/reset_password.init.js
@@ -104,7 +104,7 @@ var ResetPassword = (function () {
                     .text(text.localize('Your password has been successfully reset. ' +
                         'Please log into your account using your new password.'));
                 window.setTimeout(function () {
-                    window.location.href = page.url.url_for('login');
+                    window.location.href = page.url.url_for('user/my_accountws');
                 }, 5000);
             }
         }
