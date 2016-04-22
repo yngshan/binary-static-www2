997ddad0ead70624cf0c978bd59348d4996cfb5a
negar <negar@binary.com>
2016-04-22 14:54:50 +0800

    move return to if condition

--- src/javascript/binary/static/view_popup/view_popupws.js
+++ src/javascript/binary/static/view_popup/view_popupws.js
@@ -57,11 +57,10 @@ var ViewPopupWS = (function() {
             return;
         }
 
-        if (!document.getElementById(wrapperID)) return;
-
         $.extend(contract, response.proposal_open_contract);
 
         if(contract && contractType) {
+            if (!document.getElementById(wrapperID)) return;
             ViewPopupWS[contractType + 'Update']();
             return;
         }
