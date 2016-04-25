e35624fcb079f04246ad1f3cc5682b23ef0e13bc
qingwei <qingwei@binary.com>
2016-04-25 14:50:00 +0800

    Hide msg

--- src/javascript/binary/static/random_chg_msg.js
+++ src/javascript/binary/static/random_chg_msg.js
@@ -1,26 +1,24 @@
 // temporary script to show message about Random renamed to Volatile on trading page
 
-pjax_config_page('trading', function(){
-    return {
-        onLoad: function() {
-            var hasRandom = false;
-            Object.keys(Symbols.markets())
-                .forEach(function(s) {
-                    if (s === 'random') {
-                        hasRandom = true;
-                    }
-                });
-
-            var tempMsgKey = 'hide_temp_msg';
-            if (SessionStore.get(tempMsgKey) || !hasRandom) {
-                $('#temp_notice_msg').addClass('invisible');
+function showRandomRenamedMsg(msg) {
+    if (JSON.parse(msg.data).msg_type !== 'active_symbols') {
+        return;
+    }
+    var hasRandom = false;
+    Object.keys(Symbols.markets())
+        .forEach(function(s) {
+            if (s === 'volidx') {
+                hasRandom = true;
             }
+        });
 
-            $('#close_temp_msg').click(function() {
-                SessionStore.set(tempMsgKey, '1');
-                $('#temp_notice_msg').addClass('invisible');
-            });
-        }
-    };
+    var tempMsgKey = 'hide_temp_msg';
+    if (SessionStore.get(tempMsgKey) || !hasRandom) {
+        $('#temp_notice_msg').addClass('invisible');
+    }
 
-});
+    $('#close_temp_msg').click(function() {
+        SessionStore.set(tempMsgKey, '1');
+        $('#temp_notice_msg').addClass('invisible');
+    });
+}
