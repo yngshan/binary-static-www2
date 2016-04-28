90c377584ae1f8c05c31a8f842d50ca6db09ca3b
qingwei <qingwei@binary.com>
2016-04-28 10:24:36 +0800

    Remove original so that it get updated

--- src/javascript/binary/utility.js
+++ src/javascript/binary/utility.js
@@ -439,6 +439,8 @@ function showLocalOnHover(s) {
         var gmtTime = moment.utc(ele.innerHTML.replace('\n', ' ')).local().format('YYYY-MM-DD HH:mm:ss ZZ');
         var timeToShow = gmtTime.replace(' ', '\n');
         var tooltip = $('<span></span>', { class: 'tooltip-content', text: timeToShow });
-        $(ele).append(tooltip);
+        $(ele)
+            .remove('.tooltip-content')
+            .append(tooltip);
     });
 }
