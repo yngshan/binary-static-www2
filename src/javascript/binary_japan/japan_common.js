0ee9e76023c7708fbf0f91c1908dd4fe58788be9
binary-maksym <maksym@binary.com>
2016-05-02 15:42:12 +0800

    ...

--- src/javascript/binary_japan/japan_common.js
+++ src/javascript/binary_japan/japan_common.js
@@ -1,46 +1,50 @@
-function displayContractForms(id, elements, selected) {
-  var $menu = $('#market_menu');
+if (isJapanTrading()) {
 
-  $menu.empty();
+    var marketsOrder = function(market) {
+        var order = {
+            higherlower: 1,
+            touchnotouch: 2,
+            endsinout: 3,
+            staysinout: 4,
+        };
 
-  var markets = Object.keys(elements).filter(function(el) {
-    return marketsOrder(el);
-  }).sort(function(a, b) {
-    return marketsOrder(a) - marketsOrder(b);
-  });
+        return order[market];
+    };
+
+    var displayContractForms = function(id, elements, selected) {
+        var $menu = $('#market_menu');
+
+        $menu.empty();
+
+        var markets = Object.keys(elements).filter(function(el) {
+            return marketsOrder(el);
+        }).sort(function(a, b) {
+            return marketsOrder(a) - marketsOrder(b);
+        });
 
 
-  $.each(markets, function(key, value) {
-    $menu
-      .append(
-        $('<li />').append(
-          $("<input />")
-          .attr("type", "radio")
-          .attr("name", "market_menu")
-          .attr("value", value)
-          .attr('id', value),
-          $("<label />")
-          .text(elements[value])
-          .attr('for', value)
-        ).click());
-  });
+        $.each(markets, function(key, value) {
+            $menu
+                .append(
+                    $('<li />').append(
+                        $("<input />")
+                        .attr("type", "radio")
+                        .attr("name", "market_menu")
+                        .attr("value", value)
+                        .attr('id', value),
+                        $("<label />")
+                        .text(elements[value])
+                        .attr('for', value)
+                    ).click());
+        });
 
-  $('#' + selected).attr('checked', 'checked');
-  $menu.find('input[name=market_menu]').change(function(e) {
-    sessionStorage.setItem('formname', e.target.value);
-    processContractForm();
-    TradingAnalysis.request();
-  });
+        $('#' + selected).attr('checked', 'checked');
+        $menu.find('input[name=market_menu]').change(function(e) {
+            sessionStorage.setItem('formname', e.target.value);
+            processContractForm();
+            TradingAnalysis.request();
+        });
+    };
+
+
 }
-
-function marketsOrder(market) {
-  var order = {
-    higherlower: 1,
-    touchnotouch: 2,
-    endsinout: 3,
-    staysinout: 4,
-  };
-
-  return order[market];
-}
-
